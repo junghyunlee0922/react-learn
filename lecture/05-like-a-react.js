d0f797d719363cac59ea7cf67a29052c4a3e86d2
junghyunlee0922 <self21c123@naver.com>
2024-01-18 14:43:26 +0900

    [실습 중...] React처럼 part 2

--- lecture/05-like-a-react.js
+++ lecture/05-like-a-react.js
@@ -1,3 +1,13 @@
-import EUID from "./lib/EUID";
+import { createElement as h } from "./lib/EUID.js";
 
-console.log(EUID);
+const appElement = h(
+  "div",
+  {
+    id: "app",
+    "data-type": "application",
+  },
+  h("h1", {}, "안녕!", h("br"), "리액트."),
+  h("p", { "data-id": "slogan" }, "리액트는....")
+);
+
+console.log(appElement.outerHTML);
